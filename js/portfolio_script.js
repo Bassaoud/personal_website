7cb86669bfda2e34deee86453150742b72b21246
Bassaoud <mohamed.bassaoud@yahoo.fr>
2020-11-02 00:47:17 +0100

    personal website finished ! Thanks God

--- js/portfolio_script.js
+++ js/portfolio_script.js
@@ -4,14 +4,38 @@ $(document).ready(function(){
             $('.navbar').addClass("sticky")
         } else {
             $('.navbar').removeClass("sticky")
+        };
+
+        if(this.scrollY > 500) {
+            $('.scroll-up-btn').addClass("show")  
+        } else {
+            $('.scroll-up-btn').removeClass("show")  
         }
+    });
+
+    //slide up script 
+    $('.scroll-up-btn').click(function(){
+        $('html').animate({scrollTop: 0})
     });
     //toggle menu/navbar script
     $('.menu-btn').click(function(){
         $('.navbar .menu').toggleClass("active");
         $('.menu-btn i').toggleClass("active");
     });
+    //typing animation script
+    var typed = new Typed(".typing", {
+        strings: ["full stack developer", "front-end developer", "back-end developer", "Freelancer"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
 
+    var typed = new Typed(".typing-2", {
+        strings: ["full stack developer", "front-end developer", "back-end developer", "Freelancer"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
     // owl carousel script
     $('.carousel').owlCarousel({
         margin: 20,
